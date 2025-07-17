9fb4f1c79ebbbd07bbf171345b6b11ea4d68baa7
admin-313 <w@quantum-level.net>
2025-07-17 16:47:50 +0300

    add clickhouse and it's configs

--- compose-files/grafana-prometheus/clickhouse_config/clickhouse_on_startup.sql
+++ compose-files/grafana-prometheus/clickhouse_config/clickhouse_on_startup.sql
@@ -1,3 +1,17 @@
+SET profile = 'admin';
+
 CREATE DATABASE IF NOT EXISTS metrics;
 
 CREATE TABLE IF NOT EXISTS prometheus_metrics ENGINE=TimeSeries;
+
+CREATE ROLE IF NOT EXISTS prometheus_rw;
+CREATE ROLE IF NOT EXISTS grafana_ro;
+
+GRANT SELECT, INSERT ON metrics.prometheus_metrics TO prometheus_rw;
+GRANT SELECT ON metrics.prometheus_metrics TO grafana_ro;
+
+GRANT prometheus_rw TO prometheus;
+GRANT grafana_ro TO grafana;
+
+SET DEFAULT ROLE prometheus_rw TO prometheus;
+SET DEFAULT ROLE grafana_ro TO grafana;

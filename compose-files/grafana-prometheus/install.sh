c88044c1ba262cbb0e1ccc2be87207c5e46c2afa
admin-313 <w@quantum-level.net>
2025-08-11 11:49:47 +0300

    add initial install script

---
+++ compose-files/grafana-prometheus/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This script will install docker compose deployment of Grafana stack"
+
+echo "Now populate the following files with your configs:"
+echo "sd_configs/blackbox_targets.json"
+echo "prometheus_config/prometheus.yml"
+echo ".env"
